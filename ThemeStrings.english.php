6e922aaedfed61f96754195fd6e5e7c3343b8265
DhaYzon <dhayzon@gmail.com>
2015-08-27 23:23:50 -0500

    Create ThemeStrings.english.php

---
+++ ThemeStrings.english.php
@@ -0,0 +1,9 @@
+
+<?php
+// Version: 2.0; Themes
+// esto en el achivo idiomas ThemeStrings.english.php
+global $scripturl;
+
+$txt['enable_chat_fm'] ='Enable ChatWing';
+$txt['chat_secret'] = 'Secret Code ChatWing ';
+$txt['chat_description'] = 'Secret: This is used to encrypt the custom user session. Here is an example of the PHP implementation ';
